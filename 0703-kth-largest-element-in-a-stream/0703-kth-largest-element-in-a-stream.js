26ae1216c940a2102c23194f0bb5803771fdd491
hhheeeeee <122246429+hhheeeeee@users.noreply.github.com>
2026-01-05 17:12:57 +0900

    Time: 21 ms (81.83%), Space: 68.1 MB (83.63%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
@@ -2,36 +2,97 @@
  * @param {number} k
  * @param {number[]} nums
  */
-var KthLargest = function(k, nums) {
-    this.kth  = k
-    this.arr = nums.toSorted((a, b) => b - a)
-    if (this.arr.length >= k) {
-        this.arr = this.arr.slice(0, this.kth)
-    }
+var KthLargest = function (k, nums) {
+  this.kth = k;
+  // 이진 트리를 기반으로 한 자료구조
+  // 부모 노드가 자식들보다 작거나 같은 구조
+  // 가장 작은 값이 부모 노드에 위치하는 자료구조
+  this.minHeap = [];
+
+  for (const num of nums) {
+    this.push(num);
+  }
+
+  while (this.minHeap.length > this.kth) {
+    this.pop();
+  }
+};
+
+KthLargest.prototype.pop = function () {
+  if (this.minHeap.length === 0) return undefined;
+  if (this.minHeap.length === 1) return this.minHeap.pop();
+
+  const smallest = this.minHeap[0];
+  this.minHeap[0] = this.minHeap.pop(); 
+  this.heapifyDown();                   
+  return smallest;
+};
+
+KthLargest.prototype.push = function (val) {
+  this.minHeap.push(val);
+  this.heapifyUp();
 };
 
 
+KthLargest.prototype.heapifyUp = function () {
+  let index = this.minHeap.length - 1;
+  while (index > 0) {
+    const parentIndex = Math.floor((index - 1) / 2);
+    // 부모 인덱스에 있는 값이 더 작거나 같으면 종료
+    if (this.minHeap[parentIndex] <= this.minHeap[index]) break;
+    // 만약에 아니라면, 부모 노드와 현재 노드를 스왑
+    [this.minHeap[parentIndex], this.minHeap[index]] = [
+      this.minHeap[index],
+      this.minHeap[parentIndex],
+    ];
+    index = parentIndex;
+  }
+};
 
+KthLargest.prototype.heapifyDown = function () {
+  let index = 0;
+  while (index < this.minHeap.length) {
+    let leftChildIndex = index * 2 + 1;
+    let rightChildIndex = index * 2 + 2;
+    let smallestIndex = index;
+    // 왼쪽 자식이랑 오른쪽 자식이랑 비교해서 더 작은 자식이 smallestIndex가 됨
+    if (
+      leftChildIndex < this.minHeap.length &&
+      this.minHeap[leftChildIndex] < this.minHeap[smallestIndex]
+    ) {
+      smallestIndex = leftChildIndex;
+    }
+    if (
+      rightChildIndex < this.minHeap.length &&
+      this.minHeap[rightChildIndex] < this.minHeap[smallestIndex]
+    ) {
+      smallestIndex = rightChildIndex;
+    }
+    // 만약에 자식들 중 더 작은 값이 없다면 종료
+    if (smallestIndex === index) break;
 
+    // 만약에 자식들 중 더 작은 값이 있다면, 부모 노드와 자식 노드를 스왑
+    [this.minHeap[index], this.minHeap[smallestIndex]] = [
+      this.minHeap[smallestIndex],
+      this.minHeap[index],
+    ];
+    // 자식 노드로 이동
+    index = smallestIndex;
+  }
+};
 
-// 8, 5, 4
 
 /** 
  * @param {number} val
  * @return {number}
  */
-KthLargest.prototype.add = function(val) {
-   if (this.arr.length < this.kth) {
-    this.arr.push(val)
-    this.arr.sort((a, b) => b - a)
-   } else {
-    if (this.arr[this.kth - 1] < val) {
-      this.arr.pop()
-      this.arr.push(val)
-      this.arr.sort((a, b) => b - a)
-    }
-   }
-   return this.arr[this.kth - 1]
+KthLargest.prototype.add = function (val) {
+  this.push(val);
+
+  if (this.minHeap.length > this.kth) {
+    this.pop();
+  }
+  return this.minHeap[0];
 };
 
 /** 
