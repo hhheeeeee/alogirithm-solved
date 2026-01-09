9994a37822008a8d38489876e2aacffc6355a1fe
hhheeeeee <122246429+hhheeeeee@users.noreply.github.com>
2026-01-09 21:57:03 +0900

    Time: 1 ms (78.15%), Space: 52.2 MB (100%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -4,14 +4,15 @@
  */
 var minCostClimbingStairs = function (cost) {
     const n = cost.length;
-    const dp = new Array(n).fill(0);
-  
-    dp[0] = cost[0];
-    dp[1] = cost[1];
+    let first = cost[0]
+    let second = cost[1]
+
+    if (n <= 2) return Math.min(first, second)
   
     for (let i = 2; i < n; i++) {
-      dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
-    }
-  
-    return Math.min(dp[n - 1], dp[n - 2]);
+		let cur = cost[i] + Math.min(first, second);
+		first = second;
+		second = cur;
+	}
+	return Math.min(first, second);
   };
