9fe902e5422513ecda2aed29b3218c061c379e08
hhheeeeee <122246429+hhheeeeee@users.noreply.github.com>
2026-01-12 14:19:57 +0900

    Time: 2 ms (66.24%), Space: 59.3 MB (52.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -2,20 +2,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function (nums) {
-    let reachable = new Array(nums.length).fill(0)
+var canJump = function(nums) {
+  let maxReach = 0;
 
-    let queue = [0]
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxReach) return false;          // 여기 못 오면 끝
+    maxReach = Math.max(maxReach, i + nums[i]);
+    if (maxReach >= nums.length - 1) return true; // 이미 끝 도달 가능
+  }
 
-    while (queue.length > 0) {
-        let now = queue.shift()
-        let dist = nums[now]
-        for (let i = now; i < now + dist + 1; i++) {
-            if (i < nums.length && reachable[i] === 0) {
-                reachable[i] = 1
-                queue.push(i)
-            }
-        }
-    }
-    return reachable[nums.length - 1] === 1
+  return true;
 };
