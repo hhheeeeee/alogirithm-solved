cc4f56283b989459a32c74226d5b7f07cdc75f0f
hhheeeeee <122246429+hhheeeeee@users.noreply.github.com>
2026-01-13 23:49:40 +0900

    Time: 15 ms (17.32%), Space: 66.5 MB (20.76%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -2,19 +2,16 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function(intervals) {
+var merge = function (intervals) {
     intervals.sort((a, b) => a[0] - b[0])
-    let [start, end] = intervals[0]
-    let result = []
-     for (let i = 1; i < intervals.length; i++) {
-        const [nowStart, nowEnd] = intervals[i]; 
-        if (nowStart <= end) {
-             end = Math.max(nowEnd, end)
-        } else {
-            result.push([start, end]);
-            [start, end] = [nowStart, nowEnd];
+    for (let i = 1; i < intervals.length; i++) {
+        let [x, y] = intervals[i]
+        let [prevX, prevY] = intervals[i - 1]
+        if (x <= prevY) {
+            intervals[i - 1][1] = Math.max(y, prevY);
+            intervals.splice(i, 1)
+            i--
         }
     }
-    result.push([start, end])
-    return result
+    return intervals
 };
