6c1a1574fcfba2070f31657935f33310798bf301
hhheeeeee <122246429+hhheeeeee@users.noreply.github.com>
2026-01-21 09:12:55 +0900

    Time: 1 ms (51.18%), Space: 55 MB (21.6%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,19 +4,13 @@
  * @return {number}
  */
 var uniquePaths = function (m, n) {
+    let grid = Array.from({ length: m }, (() => Array(n).fill(1)))
 
-    let dp = Array.from({ length: m }, () => Array.from({ length: n }, () => []))
-
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (i == 0 || j == 0) {
-                dp[i][j] = 1
-            } else {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-                console.log(dp, i, j)
-            }
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
         }
     }
 
-    return dp[m - 1][n - 1]
+    return grid[m - 1][n - 1]
 };
