637541da2dc2d6b8ddefe4f6eab63890637464cd
hhheeeeee <122246429+hhheeeeee@users.noreply.github.com>
2026-01-26 23:33:56 +0900

    Time: 62 ms (68.09%), Space: 63.6 MB (40.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -5,15 +5,14 @@
 var numIslands = function (grid) {
     let islands = 0
     let [m, n] = [grid.length, grid[0].length]
-    let visited = Array.from({ length: m }, () => Array.from({ length: n }, () => [0]))
 
     const dfs = (x, y) => {
-        visited[x][y] = 1
+        grid[x][y] = 2
         let dir = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
         for (let [dx, dy] of dir) {
             let [nx, ny] = [x + dx, y + dy]
-            if (0 <= nx && nx < m && 0 <= ny && ny < n && visited[nx][ny] == 0 && grid[nx][ny] == 1) {
+            if (0 <= nx && nx < m && 0 <= ny && ny < n && grid[nx][ny] == 1) {
                 dfs(nx, ny)
             }
         }
@@ -21,7 +20,7 @@ var numIslands = function (grid) {
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (grid[i][j] == 1 && visited[i][j] == 0) {
+            if (grid[i][j] == 1) {
                 dfs(i, j)
                 islands += 1
             }
