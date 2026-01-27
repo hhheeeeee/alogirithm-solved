4c051fa4c87c9115eff273151ccd5c44fa6fbb6c
hhheeeeee <122246429+hhheeeeee@users.noreply.github.com>
2026-01-28 00:21:55 +0900

    Time: 0 ms (100%), Space: 54.3 MB (37.61%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -1,32 +1,22 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
 var search = function (nums, target) {
-    let [left, right] = [0, nums.length - 1]
-    // 어디쪽이 정렬되어 있는지 알아야됨 => 왼쪽인지 오른쪽인지
+    function binary(nums, target, s, e) {
+        if (s > e) return -1;
+        let mid = Math.floor(s + (e - s) / 2);
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2)
-        if (nums[mid] === target) return mid
-        if (nums[left] === target) return left
-        if (nums[right] === target) return right
-        console.log(left, right, mid)
+        if (nums[mid] === target) return mid;
 
-        if (nums[left] > nums[mid]) { // 1) 오른쪽은 정렬된 상태
-            if (nums[mid] < target && target < nums[right]) {
-                left = mid + 1
-            } else {
-                right = mid - 1
-            }
-        } else { // 2) 왼쪽은 정렬된 상태
-            if (nums[left] < target && target < nums[mid]) { // 2-1) 정렬된 왼쪽 안에 타겟이 있음 
-                right = mid - 1
-            } else {
-                left = mid + 1
-            }
+        if (nums[mid] >= nums[s]) {
+            if (target >= nums[s] && target <= nums[mid])
+                return binary(nums, target, s, mid - 1);
+            else
+                return binary(nums, target, mid + 1, e);
+        } else {
+            if (target >= nums[mid] && target <= nums[e])
+                return binary(nums, target, mid + 1, e);
+            else
+                return binary(nums, target, s, mid - 1);
         }
     }
-    return -1
+
+    return binary(nums, target, 0, nums.length - 1);
 };
