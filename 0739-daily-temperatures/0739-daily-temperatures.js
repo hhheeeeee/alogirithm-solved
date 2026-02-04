2550503df72ca661cf9c664b8d59a1144d8429b2
hhheeeeee <122246429+hhheeeeee@users.noreply.github.com>
2026-02-04 20:59:54 +0900

    Time: 37 ms (24.78%), Space: 81.2 MB (16.97%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -5,7 +5,7 @@
 var dailyTemperatures = function (temperatures) {
     let tmpStack = []
     let idxStack = []
-    let result = []
+    let result = new Array(temperatures.length).fill(0)
     for (let i = temperatures.length - 1; 0 <= i; i--) {
         let now = temperatures[i]
         // 감소 순열이 될 때까지 pop해
@@ -14,9 +14,9 @@ var dailyTemperatures = function (temperatures) {
             idxStack.pop()
         }
         if (tmpStack.length === 0) {
-            result.unshift(0)
+            result[i] = 0
         } else {
-            result.unshift(idxStack[idxStack.length - 1] - i)
+           result[i] = idxStack[idxStack.length - 1] - i
         }
         tmpStack.push(now)
         idxStack.push(i)
