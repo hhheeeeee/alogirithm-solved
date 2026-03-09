e74bb3e2bcb61d2dcb368142c588d30e02d4994a
hhheeeeee <122246429+hhheeeeee@users.noreply.github.com>
2026-03-09 11:44:11 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0134-gas-station/0134-gas-station.js
+++ 0134-gas-station/0134-gas-station.js
@@ -3,29 +3,21 @@
  * @param {number[]} cost
  * @return {number}
  */
-var canCompleteCircuit = function (gas, cost) {
-    let n = gas.length
-
-
-    for (let i = 0; i < n; i++) {
-        if (i !== 0 && gas[i] - cost[i] <= 0) continue
-        // i를 시작으로 잡고 사이클 돌기 시작
-        let tank = gas[i]
-        let now = i
-        let startIndex = i
-        let flag = 0
-        for (let j = Math.floor((i + 1) % n); j <= Math.floor(i % n) + n; j++) {
-            let next = Math.floor(j % n)
-            if (tank - cost[now] < 0) {
-                flag = 1
-                break
-            }
-            tank = tank - cost[now] + gas[next]
-            now = next
-        }
-        // break에 한번도 안걸렸을 때!
-        if (flag == 0) return startIndex
+vvar canCompleteCircuit = function(gas, cost) {
+    if (gas.reduce((a, b) => a + b, 0) < cost.reduce((a, b) => a + b, 0)) {
+        return -1;
     }
 
-    return -1
+    let currentGas = 0;
+    let start = 0;
+
+    for (let i = 0; i < gas.length; i++) {
+        currentGas += gas[i] - cost[i];
+        if (currentGas < 0) {
+            currentGas = 0;
+            start = i + 1;
+        }
+    }
+
+    return start;    
 };
