58ee51ae899c8d4f8374877590999f123c8198d8
hhheeeeee <122246429+hhheeeeee@users.noreply.github.com>
2026-04-06 19:36:25 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,24 +3,14 @@
  * @return {number}
  */
 var jump = function (nums) {
-    let memo = new Array(nums.length).fill("Infinity")
-    
-    if (nums.length < 2) return 0
-    
-    for (let start = 0; start <= nums[0]; start++) {
-        memo[start] = 1
-    }
-
-    for (let i = 1; i < nums.length; i++) {
-        let cur = nums[i]
-        for (let j = 1; j < cur + 1; j++) {
-            if (i + j < nums.length) {
-                memo[i + j] = Math.min(memo[i] + 1, memo[i + j])
-                if (i + j === nums.length - 1) {
-                    return memo[nums.length - 1]
-                }
-            }
+    let len = nums.length - 1
+    let curr = -1, next = 0, ans = 0
+    for (let i = 0; next < len; i++) {
+        if (i > curr) {
+            ans++
+            curr = next
         }
+        next = Math.max(next, nums[i] + i)
     }
-    return memo[nums.length - 1]
+    return ans;
 };
